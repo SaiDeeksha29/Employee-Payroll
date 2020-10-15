156d2dd28c9cb34299994ba68fe3b1433d20b7a1
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-15 16:50:09 +0530

    Added:Welcome message

---
+++ main/java/com/blz/assignment/EmployeePayrollService.java
@@ -0,0 +1,14 @@
+package com.blz.assignment;
+
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+
+public class EmployeePayrollService {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Payroll problem");
+
+	}
+}

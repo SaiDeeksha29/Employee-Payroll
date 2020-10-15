65e6c0a5c2951cf66cd22fdf3955e9f1046cfcec
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-15 18:00:59 +0530

    Added:Test file for checking various file operations

---
+++ main/java/com/blz/assignment/FileUtils.java
@@ -0,0 +1,15 @@
+package com.blz.assignment;
+
+import java.io.*;
+
+public class FileUtils {
+	public static boolean deleteFiles(File deleteContents) {
+		File[] allContents = deleteContents.listFiles();
+		if (allContents != null) {
+			for (File file : allContents) {
+				deleteFiles(file);
+			}
+		}
+		return deleteContents.delete();
+	}
+}
